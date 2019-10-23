3deea8314887b13e536e703304d1ca152c038845
toufani1515 <toufani1515@gmail.com>
2019-10-23 21:09:00 +0800

    fix something

--- manage.py
+++ manage.py
@@ -4,9 +4,11 @@ from flask_migrate import MigrateCommand, Migrate
 from flask_script import Manager
 from flask_marshmallow import Marshmallow
 from flask_restful import Resource, Api
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 app.config["TESTING"] = False
 
 """
